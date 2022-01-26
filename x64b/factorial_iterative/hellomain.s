1b98fbee36d8197fe60fca36464544d6346cb0b8
Сергей Р <49802464+ihtiandr9@users.noreply.github.com>
2022-01-26 10:03:04 +0800

    initcommit

---
+++ x64b/factorial_iterative/hellomain.s
@@ -0,0 +1,78 @@
+.text
+
+.global main
+
+main:
+	push	%rbp
+	mov	%rsp,	%rbp
+	sub	$32,	%rsp
+	mov	%rdi,	-8(%rbp)
+##        lea     message, %rsi      # address of string to output
+        mov	$message, %rdi      # address of string to output
+        call    print_str
+        mov	$4, %rdi
+        call	fact
+        mov	%rax, -16(%rbp)
+        mov	$fmt_fact, %rdi
+        mov	-16(%rbp), %rsi
+        xor	%rax, %rax
+        call	printf
+	leave
+	xor	%rax,	%rax
+	ret
+
+#        # exit(0)
+#        xor     %rdi, %rdi              # we want return code 0
+#        call    exit                         # invoke operating system to exit
+#                                        # invoke operating system to exit
+
+print_str:
+        # write(1, message, 13)
+        push	%rbp
+        mov     %rsp, %rbp
+        sub	 $32, %rsp
+        mov	%rdi, -8(%rbp)
+        mov	%rsi, -16(%rbp)
+        mov	-8(%rbp),%rax
+        mov     $13, %rdx
+        mov     %rax, %rsi
+        mov     $1, %rdi                # file handle 1 is stdout
+        xor	%rax,	%rax
+        call    write                     # invoke operating system to do the write
+        leave
+        ret
+
+fact:
+        # write(1, message, 13)
+        push	%rbp
+        mov     %rsp, %rbp
+        sub	 $32, %rsp
+        mov	%rdi, -8(%rbp)
+        mov	%rcx, -16(%rbp)
+        mov	-8(%rbp), %rax
+        cmp	$0, %rax
+        jz	ret1
+        jb	err_arg
+        mov	%rax,%rcx
+cycle1:
+        dec	%rcx
+        jz	end_counter
+        mull	%ecx
+        jmp	cycle1
+end_counter:
+	mov	-16(%rbp), %rcx
+        jmp endif
+ret1:
+	mov $1, %rax
+	jmp endif
+err_arg:
+	mov $0, %rax
+endif:
+        leave
+        ret
+
+.data
+message:
+        .ascii  "Hello, world\n"
+fmt_fact:
+	.ascii "factorial is %d\n"
