55eb275f413bb923c274f67f4b343028d8d84c01
ihtiandr9 (Debian x86) <lomaster@ihtiandr9.fvds.ru>
2024-01-12 12:56:21 +0800

    feature/bookworm32: 32bit helloworld

--- x86/helloworld/hellomain.s
+++ x86/helloworld/hellomain.s
@@ -3,35 +3,28 @@
 .global main
 
 main:
-	push	%rbp
-	mov	%rsp,	%rbp
-	sub	$16,	%rsp
-	mov	%rdi,	-8(%rbp)
-##        lea     message, %rsi      # address of string to output
-        mov	$message, %rdi      # address of string to output
+	push	%ebp
+	mov	%esp,	%ebp
+	sub	$16,	%esp
+        lea     message, %eax       # address of string to output
+	push	%eax
         call    myfunc
+	add     $4, %esp
 	leave
-	xor	%rax,	%rax
+	xor	%eax,	%eax
 	ret
-        # exit(0)
-        xor     %rdi, %rdi              # we want return code 0
-        call    exit                         # invoke operating system to exit
-                                        # invoke operating system to exit
 
 myfunc:
-        # write(1, message, 13)
-        push	%rbp
-        mov     %rsp, %rbp
-        sub	 $32, %rsp
-        mov	%rdi, -8(%rbp)
-        mov	%rsi, -16(%rbp)
-        mov	-8(%rbp),%rax
-        mov     $13, %rdx
-        mov     %rax, %rsi
-        mov     $1, %rdi                # file handle 1 is stdout
-        xor	%rax,	%rax
-        call    write                     # invoke operating system to do the write
+        push	%ebp
+        mov     %esp, %ebp
+        mov	8(%ebp), %eax
+	push	$13
+	push	%eax
+	push	$1
+	# write(1, message, 13)
+        call    write               # invoke operating system to do the write
         leave
+	xor	%eax,	%eax
         ret
 
 
